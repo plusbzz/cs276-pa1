b6f58aa02e6e3b01d8df5f4e2fe4f5e43b23d830
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-04-15 09:29:42 -0400

    Attempt at extra credit

---
+++ extra_credit/index.py
@@ -0,0 +1,241 @@
+#!/bin/env python
+from collections import deque
+import os, glob, os.path
+import sys
+import re
+from itertools import groupby
+from math import *
+from struct import *
+
+if len(sys.argv) != 3:
+  print >> sys.stderr, 'usage: python index.py data_dir output_dir' 
+  os._exit(-1)
+
+total_file_count = 0
+root = sys.argv[1]
+out_dir = sys.argv[2]
+if not os.path.exists(out_dir):
+  os.makedirs(out_dir)
+
+# this is the actual posting lists dictionary
+# word id -> {position_in_file, doc freq}
+posting_dict = {}
+# this is a dict holding document name -> doc_id
+doc_id_dict = {}
+# this is a dict holding word -> word_id
+word_dict = {}
+# this is a queue holding block names, later used for merging blocks
+block_q = deque([])
+
+# function to count number of files in collection
+def count_file():
+  global total_file_count
+  total_file_count += 1
+
+# function for printing a line in a postings list to a given file
+# a useful function is f.tell(), which gives you the offset from beginning of file
+# you may also want to consider storing the file position and doc frequence in posting_dict in this call
+def print_posting(file, posting_line):
+  print >> file,posting_line
+
+#########################
+
+'''
+Format - one posting per line
+
+term_id:doc_id1,doc_id2...
+'''
+def print_postings_to_file(term_doc_list, fname):
+  with open(fname, 'wb') as f:
+    for key,group in groupby(term_doc_list,lambda tup: tup[0]):
+      docs = [str(d) for d in sorted(list(set([tup[1] for tup in group])))]
+      posting_line = str(key) + ":" + ",".join(docs)
+      print >> f, posting_line.strip()
+      
+def gammaEncodeNumber(number):
+  s = bin(number)
+  s = s[3:]
+  s = '1'*len(s)+'0'+s
+  return s
+      
+def gammaEncodeNumbers(numbers):
+  encodedNumbers = ""
+  for number in numbers:
+      encodedNumbers += gammaEncodeNumber(number)
+  
+  sz = len(encodedNumbers)
+  padsize = 8 - (sz % 8)
+  encodedNumbers = '0'*padsize + encodedNumbers
+  # divide string into list of 'bytes'
+  encodedGaps = []
+  for start in xrange(0,sz,8):
+    byte = encodedNumbers[start:(start+8)]
+    byte = int(byte,2)
+    encodedGaps.append(byte)    
+
+  return encodedGaps
+
+def generateGaps(docIdList):
+    gaps = []
+    previous = 0
+    for docId in docIdList:
+        gaps.append(docId-previous)
+        previous = docId
+        
+    return gaps
+
+def writeGammaEncodedGaps(strLine,outputFile,postingDictionary):
+    term,docsId = strLine.split(":")
+    term   = int(term)
+    docsId = sorted([int(d) for d in docsId.strip().split(",")])
+    
+    position = outputFile.tell()
+    postingDictionary[term] = (position,len(docsId))
+    
+    gaps        = generateGaps(docsId)
+    encodedGaps = gammaEncodeNumbers(gaps)
+    
+    #print >> sys.stderr, "Writing to file %s the code: %d in byte %d" % (outputfile,number,position)
+    # TODO: Find a way to write a block of bytes instead of writing byte per byte
+    for number in encodedGaps:
+      outputFile.write(chr(number))        
+
+def writeNaive(strLine,outputFile,postingDictionary):
+  if postingDictionary is not None:
+    term,docsId = strLine.split(":")
+    position = outputFile.tell()
+    postingDictionary[term] = (position,len(docsId))
+  print >> outputFile, strLine
+  
+
+def mergeBlocks(b1,b2,outputFile,postingDictionary,writeFunc):
+  with open(b1,'r') as b1_f, open(b2,'r') as b2_f, open(outputFile,'wb') as outputFile_f:
+    # (provide implementation merging the two blocks of posting lists)
+    # write the new merged posting lists block to file 'outputFile_f'
+    line1 = b1_f.readline().strip()
+    line2 = b2_f.readline().strip()
+
+    while True:     
+      if line1 == "" and line2 == "":
+        break
+      elif line1 == "": # at least one of the files is done
+        writeFunc(line2,outputFile_f,postingDictionary)    
+        for line in b2_f: writeFunc(line.strip(),outputFile_f,postingDictionary)
+        break
+    
+      elif line2 == "": # at least one of the files is done
+        writeFunc(line1,outputFile_f,postingDictionary)    
+        for line in b1_f: writeFunc(line.strip(),outputFile_f,postingDictionary)
+        break
+    
+      else:  # files have stuff in them
+        term1,docs1 = line1.split(":")
+        term2,docs2 = line2.split(":")
+        term1 = int(term1)
+        term2 = int(term2)
+  
+        # We now have two sorted lists
+        if term1 == term2: 
+          docs1 = [int(d) for d in docs1.strip().split(",")]
+          docs2 = [int(d) for d in docs2.strip().split(",")]
+          docs  = [str(d) for d in sorted(docs1+docs2)]
+          
+          line = str(term1) + ":" + ",".join(docs)
+          writeFunc(line.strip(),outputFile_f,postingDictionary)
+          
+          line1 = b1_f.readline().strip()
+          line2 = b2_f.readline().strip()
+          
+        elif term1 < term2:
+          writeFunc(line1.strip(),outputFile_f,postingDictionary)
+          line1 = b1_f.readline().strip()
+          
+        else:
+          writeFunc(line2.strip(),outputFile_f,postingDictionary)
+          line2 = b2_f.readline().strip()
+
+
+
+#########################
+
+doc_id = -1
+word_id = 0
+
+for dir in sorted(os.listdir(root)):
+  print >> sys.stderr, 'processing dir: ' + dir
+  dir_name = os.path.join(root, dir) 
+  term_doc_list = []
+  
+  for f in sorted(os.listdir(dir_name)):
+    count_file()
+    
+    # Add 'dir/filename' to doc id dictionary
+    file_name = os.path.join(dir, f)
+    doc_id += 1
+    doc_id_dict[file_name] = doc_id
+    fullpath = os.path.join(dir_name, f)
+    
+    with open(fullpath, 'r') as infile:
+      for line in infile.readlines():
+        tokens = line.strip().split()
+        for token in tokens:
+          if token not in word_dict:
+            word_dict[token] = word_id
+            word_id += 1
+          term_doc_list.append( (word_dict[token], doc_id) )
+  
+  # sort term doc list
+  print >> sys.stderr, 'sorting term doc list for dir:' + dir
+  term_doc_list.sort(key=lambda tup: tup[0])  # sorts in place
+  
+  # write the posting lists to block_pl for this current block 
+  print >> sys.stderr, 'print posting list to disc for dir:' + dir
+  # (you need to provide implementation)
+  block_pl_name = out_dir+'/'+dir
+  print_postings_to_file(term_doc_list,block_pl_name)
+
+  # append block names to a queue, later used in merging
+  block_q.append(block_pl_name)
+
+print >> sys.stderr, '######\nposting list construction finished!\n##########'
+print >> sys.stderr, '\nMerging postings...'
+
+dircnt = 1
+while len(block_q) > 1:
+  b1 = block_q.popleft()
+  b2 = block_q.popleft()
+  b_comb = '_tmp_'+str(dircnt)
+  comb = out_dir + '/'+ b_comb
+  dircnt += 1
+  
+  print >> sys.stderr, 'merging %s and %s into %s' % (b1, b2,b_comb)
+  
+  if len(block_q) > 0:
+    mergeBlocks(b1,b2,comb,None,writeNaive)
+  else:
+    # These are the last two blocks, generate a compressed/encoded corpus index
+    mergeBlocks(b1,b2,comb,posting_dict,writeGammaEncodedGaps)
+  
+  os.remove(b1)
+  os.remove(b2)
+  block_q.append(comb)
+    
+
+# Rename the final merged block to corpus.index    
+final_name = block_q.popleft()
+os.rename(final_name, out_dir+'/corpus.index')
+
+print >> sys.stderr, '\nPosting Lists Merging DONE!'
+
+# Write to file word and doc dictionary files
+doc_dict_f = open(out_dir + '/doc.dict', 'w')
+word_dict_f = open(out_dir + '/word.dict', 'w')
+posting_dict_f = open(out_dir + '/posting.dict', 'w')
+print >> doc_dict_f, '\n'.join( ['%s\t%d' % (k,v) for (k,v) in sorted(doc_id_dict.iteritems(), key=lambda(k,v):v)])
+print >> word_dict_f, '\n'.join( ['%s\t%d' % (k,v) for (k,v) in sorted(word_dict.iteritems(), key=lambda(k,v):v)])
+print >> posting_dict_f, '\n'.join(['%s\t%s' % (k,'\t'.join([str(elm) for elm in v])) for (k,v) in sorted(posting_dict.iteritems(), key=lambda(k,v):v)])
+doc_dict_f.close()
+word_dict_f.close()
+posting_dict_f.close()
+
+print total_file_count
---
+++ extra_credit/query.py
@@ -0,0 +1,161 @@
+#!/bin/env python
+from collections import deque
+import os, glob, os.path
+import sys
+import re
+
+if len(sys.argv) != 2:
+  print >> sys.stderr, 'usage: python query.py index_dir' 
+  os._exit(-1)
+
+def merge_posting (postings1, postings2):
+  new_posting = sorted(list(set(postings1).intersection(set(postings2))))
+  return new_posting
+
+# file locate of all the index related files
+index_dir = sys.argv[1]
+index_f = open(index_dir+'/corpus.index', 'rb')
+word_dict_f = open(index_dir+'/word.dict', 'r')
+doc_dict_f = open(index_dir+'/doc.dict', 'r')
+posting_dict_f = open(index_dir+'/posting.dict', 'r')
+
+word_dict = {}
+doc_id_dict = {}
+file_pos_dict = {}
+doc_freq_dict = {}
+
+print >> sys.stderr, 'loading word dict'
+for line in word_dict_f.readlines():
+  parts = line.split('\t')
+  word_dict[parts[0]] = int(parts[1])
+
+print >> sys.stderr, 'loading doc dict'
+for line in doc_dict_f.readlines():
+  parts = line.split('\t')
+  doc_id_dict[int(parts[1])] = parts[0]
+
+print >> sys.stderr, 'loading index'
+for line in posting_dict_f.readlines():
+  parts = line.split('\t')
+  term_id = int(parts[0])
+  file_pos = int(parts[1])
+  doc_freq = int(parts[2])
+  file_pos_dict[term_id] = file_pos
+  doc_freq_dict[term_id] = doc_freq
+
+
+# corpus.index is compressed; the length in bytes of the postings list for word_n is calculated:
+# file_pos_dict[word_n+1] - file_pos_dict[word_n]
+# If word_n corresponds with the largest word_id in the dictionary then word_n+1 = total_bytes_in_index
+total_bytes_in_index = os.path.getsize(index_dir+'/corpus.index')
+last_term_id = len(word_dict) - 1
+
+
+def gammaDecodeArray(byteArray):
+  decodedGaps = [];
+  decodedGapStr = ""
+  
+  for byte in byteArray:
+    decodedGapStr += bin(byte)[2:]
+  
+  strptr = 0
+  sz = len(decodedGapStr)
+  while strptr < sz:
+    count = 0
+    while strptr < sz and decodedGapStr[strptr] == "1":
+      strptr += 1
+      count += 1
+    
+    strptr += 1
+    gap = '1'+decodedGapStr[strptr:(strptr+count)]
+    strptr += count
+    decodedGaps.append(int(gap,2))
+  
+  print decodedGaps          
+  return decodedGaps
+
+def generateDocIds(gapList):
+    docIds = []
+    previous = 0
+    for gap in gapList:
+        previous = gap + previous
+        docIds.append(previous)
+        
+    return docIds
+  
+def readCompressedIndex(index_f,wordId,file_pos,numberOfBytes):
+  index_f.seek(file_pos)
+  
+  encodedGaps = bytearray(index_f.read(numberOfBytes))
+  decodedGaps = gammaDecodeArray(encodedGaps)
+  docIds      = generateDocIds(decodedGaps)
+  
+  return str(wordId) + ":" + ",".join([str(d) for d in docIds])
+  
+
+# provide implementation for posting list lookup for a given term
+# a useful function to use is index_f.seek(file_pos), which does a disc seek to 
+# a position offset 'file_pos' from the beginning of the file
+
+def read_posting(term):
+  if term in word_dict:
+    term_id = word_dict[term]
+    file_pos = file_pos_dict[term_id]
+    positionNextTerm = 0
+    
+    if term_id == last_term_id:
+      positionNextTerm = total_bytes_in_index
+    else:
+      positionNextTerm = file_pos_dict[term_id+1]
+      
+    gapListLengthInBytes = positionNextTerm - file_pos
+    postings = readCompressedIndex(index_f,term_id,file_pos,gapListLengthInBytes)
+    
+    word_id,docs = postings.strip().split(':')
+    posting_list = [int(d) for d in docs.split(',')]
+    doc_freq = doc_freq_dict[term_id]
+    return (doc_freq,posting_list)
+  else:
+    return None
+
+# read query from stdin
+while True:
+  input = sys.stdin.readline()
+  input = input.strip()
+  if len(input) == 0: # end of file reached
+    break
+  input_parts = input.split()
+  # you need to translate words into word_ids
+  # don't forget to handle the case where query contains unseen words
+  no_results = False
+  postings = []
+  for term in input_parts:    
+    posting = read_posting(term)
+    
+    # Account for unseen words
+    if posting is None:
+      no_results = True
+      break
+    
+    postings.append(posting)
+
+  if no_results:
+    print "no results found"
+    continue
+  
+    
+  # Sort postings by size increasing order
+  postings.sort(key=lambda tup: tup[0])
+  postings = [p[1] for p in postings]
+  
+  # merge the posting lists to produce the final result
+  result = reduce(merge_posting,postings)
+
+  if len(result) == 0:  
+    print "no results found"
+    continue
+
+  # don't forget to convert doc_id back to doc_name, and sort in lexicographical order
+  # before printing out to stdout
+  docs = sorted([doc_id_dict[doc_id] for doc_id in result])
+  for doc in docs: print doc  
